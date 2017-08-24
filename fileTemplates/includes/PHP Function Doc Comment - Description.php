674e78271e8b103c525a15cc037643d6301f39b9
terry <terry@perk.com>
2017-08-24 12:49:17 -0500

    PS-2017.2.1 <terrymatula@Terrys-MacBook-Pro-17.local Create PSR2.xml, markdown.xml, PHP Class.php, IntelliLang.xml, project.default.xml, diff.xml, Default copy.xml, html5.php, Laravel.xml, Default.xml, file.template.settings.xml, ignore.xml, find.xml, Html5.html, BladeView༏ext༏.blade.php, code.style.schemes.xml, github_settings.xml, Default.xml, PHP.xml, Matula_80s.icls, JavaScript.xml, -cs-fixer over current class.xml, Brightscript.xml, Zen XSL.xml, editor.codeinsight.xml, Matula_Gloom.icls, keymap.xml, editor.xml, applicationLibraries.xml, debugger.xml, PHP File.php, baseRefactoring.xml, ide.general.xml, Matula_TomNight.icls, git.xml, other.xml, Zen CSS.xml, xpath.xml, ui.lnf.xml, filetypes.xml, php.xml, colors.scheme.xml, html_xml.xml, notifications.xml, PHP Trait.php, SQL.xml, PHPUnit Test.php, jdk.table.xml, PHP Function Doc Comment - Description.php, laf.xml, Mac OS X 10_5_ copy.xml, web-browsers.xml, vcs.xml, PHP Interface.php, databaseSettings.xml

---
+++ fileTemplates/includes/PHP Function Doc Comment - Description.php
@@ -0,0 +1,8 @@
+/**
+* ${NAME}
+*
+${PARAM_DOC}
+#if (${TYPE_HINT} != "void") * @return ${TYPE_HINT}
+#end
+${THROWS_DOC}
+*/
---
+++ fileTemplates/internal/PHPUnit Test.php
@@ -0,0 +1,7 @@
+<?php
+#if (${NAMESPACE})
+namespace ${NAMESPACE};
+#end
+class ${NAME} extends#if(${NAMESPACE}) \PHPUnit_Framework_TestCase #else PHPUnit_Framework_TestCase #end{
+
+}
